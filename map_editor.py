6a2b6afc0fb7079db47bd2236fbe84c7a0fc32f8
DonovanMMM <124104944+DonovanMMM@users.noreply.github.com>
2023-05-07 09:40:20 -0600

    Added amongus

--- map_editor.py
+++ map_editor.py
@@ -2,6 +2,9 @@ import pygame
 
 pygame.init()
 
+FPS = 60
+clock = pygame.time.Clock()
+
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 640
 LOWER_MARGIN = 100
@@ -9,6 +12,10 @@ SIDE_MARGIN = 300
 
 screen = pygame.display.set_mode((SCREEN_WIDTH + SIDE_MARGIN, SCREEN_HEIGHT + LOWER_MARGIN))
 pygame.display.set_caption("Level Editor")
+
+ROWS = 16
+MAX_COLUMNS = 150
+TILE_SIZE = SCREEN_HEIGHT // ROWS
 
 scroll_left = False
 scroll_right = False
@@ -24,23 +31,34 @@ GREEN = (144, 201, 120)
 WHITE = (255, 255, 255)
 RED = (200, 25, 25)
 
+
 def draw_bg():
     screen.fill(GREEN)
-    screen.blit(sky_img, (-scroll, 0))
-    screen.blit(mountain_img, (-scroll, SCREEN_HEIGHT - mountain_img.get_height() - 300))
-    screen.blit(pine1_img, (-scroll, SCREEN_HEIGHT - pine1_img.get_height() - 150))
-    screen.blit(pine1_img, (-scroll, SCREEN_HEIGHT - pine1_img.get_height()))
+    width = sky_img.get_width()
+    for x in range(4):
+        screen.blit(sky_img, ((x * width) - scroll * 0.5, 0))
+        screen.blit(mountain_img, ((x * width) - scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
+        screen.blit(pine1_img, ((x * width) - scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
+        screen.blit(pine1_img, ((x * width) - scroll * 0.8, SCREEN_HEIGHT - pine1_img.get_height()))
 
+
+def draw_grid():
+    for c in range(MAX_COLUMNS + 1):
+        pygame.draw.line(screen, WHITE, (c * TILE_SIZE - scroll, 0), (c * TILE_SIZE - scroll, SCREEN_HEIGHT))
+    for c in range(ROWS + 1):
+        pygame.draw.line(screen, WHITE, (0, c * TILE_SIZE), (SCREEN_WIDTH, c * TILE_SIZE))
 
 
 run = True
 while run:
+    clock.tick(FPS)
     draw_bg()
+    draw_grid()
 
-    if scroll_left == True:
-        scroll -= 5
-    if scroll_right == True:
-        scroll += 5
+    if scroll_left and scroll > 0:
+        scroll -= 5 * scroll_speed
+    if scroll_right:
+        scroll += 5 * scroll_speed
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,6 +69,8 @@ while run:
                 scroll_left = True
             if event.key == pygame.K_d:
                 scroll_right = True
+            if event.key == pygame.K_LSHIFT:
+                scroll_speed = 5
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
         if event.type == pygame.KEYUP:
@@ -58,7 +78,8 @@ while run:
                 scroll_left = False
             if event.key == pygame.K_d:
                 scroll_right = False
-
+            if event.key == pygame.K_LSHIFT:
+                scroll_speed = 1
 
     pygame.display.update()
 
